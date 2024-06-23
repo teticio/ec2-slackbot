1f6a643c0286bd2f0edac5658f2f3f3b6eb51a91
Robert Dargavel Smith <teticio@gmail.com>
2024-06-23 23:18:33 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,355 @@
+# TODO:
+# attach EBS volume
+# warnings about long-running instances
+
+import json
+import os
+
+import boto3
+import yaml
+from flask import Flask, Response, abort, jsonify, request
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
+
+from ec2 import launch_ec2_instance
+
+config = yaml.safe_load(open("config.yaml"))
+
+app = Flask(__name__)
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+verifier = SignatureVerifier(os.environ["SLACK_SIGNING_SECRET"])
+ec2_client = boto3.client("ec2", region_name=config["region"])
+
+
+@app.before_request
+def verify_slack_signature():
+    if request.path.startswith("/slack/"):
+        if not verifier.is_valid_request(request.get_data(), request.headers):
+            abort(400, "Invalid Slack signature")
+
+
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    if request.content_type == "application/json":
+        data = request.json
+    elif request.content_type == "application/x-www-form-urlencoded":
+        data = request.form
+    else:
+        return "Unsupported Media Type", 415
+
+    if "payload" in data:
+        payload = json.loads(data["payload"])
+        event_type = payload.get("type")
+
+        if event_type == "view_submission":
+            handle_interactions(payload)
+
+    if "challenge" in data:
+        return jsonify({"challenge": data["challenge"]})
+
+    return Response(status=200)
+
+
+@app.route("/slack/commands", methods=["POST"])
+def handle_commands():
+    data = request.form
+    command = data.get("command")
+    sub_command = data.get("text")
+    trigger_id = data.get("trigger_id")
+    user_name = data.get("user_name")
+
+    if command == "/ec2":
+        if sub_command == "key":
+            return open_key_modal(trigger_id)
+
+        elif sub_command == "up":
+            try:
+                ec2_client.describe_key_pairs(KeyNames=[user_name])
+            except ec2_client.exceptions.ClientError:
+                return (
+                    jsonify(
+                        response_type="ephemeral",
+                        text="Please upload your public key first with /ec2 key.",
+                    ),
+                    200,
+                )
+            return open_instance_launch_modal(trigger_id)
+
+        elif sub_command == "down":
+            return open_instance_terminate_modal(trigger_id, user_name)
+
+        else:
+            return (
+                jsonify(
+                    response_type="ephemeral",
+                    text="Command must be one of: key, up, down.",
+                ),
+                200,
+            )
+
+    return jsonify(response_type="ephemeral", text="Command not recognized."), 200
+
+
+def open_key_modal(trigger_id):
+    modal = {
+        "type": "modal",
+        "callback_id": "submit_key",
+        "title": {"type": "plain_text", "text": "Upload EC2 Key"},
+        "submit": {"type": "plain_text", "text": "Submit"},
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "key_input",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "public_key",
+                    "multiline": True,
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Paste your SSH public key here.",
+                    },
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Public Key",
+                },
+            }
+        ],
+    }
+
+    try:
+        client.views_open(trigger_id=trigger_id, view=modal)
+    except SlackApiError as e:
+        print(f"Error opening modal: {e.response['error']}")
+
+    return Response(status=200)
+
+
+def open_instance_launch_modal(trigger_id):
+    ami_options = [
+        {"text": {"type": "plain_text", "text": ami["name"]}, "value": ami["id"]}
+        for ami in config["amis"]
+    ]
+    instance_type_options = [
+        {"text": {"type": "plain_text", "text": type}, "value": type}
+        for type in config["instance_types"]
+    ]
+    modal = {
+        "type": "modal",
+        "callback_id": "launch_instance",
+        "title": {"type": "plain_text", "text": "Launch EC2 Instance"},
+        "submit": {"type": "plain_text", "text": "Launch"},
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "ami_choice",
+                "element": {
+                    "type": "static_select",
+                    "action_id": "ami",
+                    "placeholder": {"type": "plain_text", "text": "Select an AMI"},
+                    "options": ami_options,
+                },
+                "label": {"type": "plain_text", "text": "AMI"},
+            },
+            {
+                "type": "input",
+                "block_id": "instance_type_choice",
+                "element": {
+                    "type": "static_select",
+                    "action_id": "instance_type",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select Instance Type",
+                    },
+                    "options": instance_type_options,
+                },
+                "label": {"type": "plain_text", "text": "Instance Type"},
+            },
+            {
+                "type": "section",
+                "block_id": "options",
+                "text": {"type": "mrkdwn", "text": "Options"},
+                "accessory": {
+                    "type": "checkboxes",
+                    "action_id": "efs_mount_check",
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Mount SageMaker Studio EFS",
+                            },
+                            "value": "mount_efs",
+                        }
+                    ],
+                },
+            },
+            {
+                "type": "input",
+                "block_id": "startup_script",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "startup_script_input",
+                    "multiline": True,
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Enter startup script (optional)",
+                    },
+                },
+                "label": {"type": "plain_text", "text": "Startup Script (Optional)"},
+                "optional": True,
+            },
+        ],
+    }
+
+    try:
+        client.views_open(trigger_id=trigger_id, view=modal)
+    except SlackApiError as e:
+        return jsonify(status=500, text=f"Failed to open modal: {e.response['error']}")
+
+    return Response(status=200)
+
+
+def open_instance_terminate_modal(trigger_id, user_name):
+    instances = ec2_client.describe_instances(
+        Filters=[
+            {"Name": "tag:User", "Values": [user_name]},
+            {"Name": "instance-state-name", "Values": ["pending", "running"]},
+        ]
+    )
+    instance_options = [
+        {
+            "text": {"type": "plain_text", "text": instance["InstanceId"]},
+            "value": instance["InstanceId"],
+        }
+        for reservation in instances["Reservations"]
+        for instance in reservation["Instances"]
+    ]
+    if len(instance_options) == 0:
+        return (
+            jsonify(response_type="ephemeral", text="No instances to terminate."),
+            200,
+        )
+
+    modal = {
+        "type": "modal",
+        "callback_id": "terminate_instance",
+        "title": {"type": "plain_text", "text": "Terminate EC2 Instances"},
+        "submit": {"type": "plain_text", "text": "Terminate"},
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "instance_selection",
+                "element": {
+                    "type": "checkboxes",
+                    "action_id": "selected_instances",
+                    "options": instance_options,
+                },
+                "label": {"type": "plain_text", "text": "Select Instances"},
+            }
+        ],
+    }
+
+    try:
+        client.views_open(trigger_id=trigger_id, view=modal)
+    except SlackApiError as e:
+        return jsonify(status=500, text=f"Failed to open modal: {e.response['error']}")
+
+    return Response(status=200)
+
+
+def handle_interactions(payload):
+    user_id = payload["user"]["id"]
+    user_name = payload["user"]["username"]
+    callback_id = payload.get("view", {}).get("callback_id")
+    values = payload.get("view", {}).get("state", {}).get("values")
+
+    if callback_id == "submit_key":
+        public_key = values["key_input"]["public_key"]["value"]
+        handle_key_submission(user_id, public_key)
+
+    elif callback_id == "launch_instance":
+        ami_id = values["ami_choice"]["ami"]["selected_option"]["value"]
+        instance_type = values["instance_type_choice"]["instance_type"][
+            "selected_option"
+        ]["value"]
+        mount_efs = any(
+            option["value"] == "mount_efs"
+            for option in values["options"]["efs_mount_check"]["selected_options"]
+        )
+        startup_script = values["startup_script"]["startup_script_input"]["value"]
+        handle_instance_launch(
+            user_id, user_name, ami_id, instance_type, mount_efs, startup_script
+        )
+
+    elif callback_id == "terminate_instance":
+        selected_instances = [
+            option["value"]
+            for option in values["instance_selection"]["selected_instances"][
+                "selected_options"
+            ]
+        ]
+        handle_instance_termination(user_id, selected_instances)
+
+
+def handle_key_submission(user_id, public_key):
+    try:
+        ec2_client.delete_key_pair(KeyName=user_id)
+    except ec2_client.exceptions.ClientError as e:
+        pass
+
+    try:
+        ec2_client.import_key_pair(KeyName=user_id, PublicKeyMaterial=public_key)
+        client.chat_postMessage(
+            channel=user_id, text="Public key has been updated successfully."
+        )
+    except ec2_client.exceptions.ClientError as e:
+        client.chat_postMessage(
+            channel=user_id,
+            text=f"Failed to store public key: {e.response['Error']['Message']}",
+        )
+
+
+def handle_instance_launch(
+    user_id, user_name, ami_id, instance_type, mount_efs, startup_script
+):
+    try:
+        instance = launch_ec2_instance(
+            ec2_client=ec2_client,
+            ami_id=ami_id,
+            iam_instance_profile=config["iam_instance_profile"],
+            instance_type=instance_type,
+            key_name=user_name,
+            security_group_ids=config["security_group_ids"],
+            subnet_id=config["subnet_id"],
+            efs_ip=config["efs_ip"] if mount_efs else None,
+            sms_user_name=user_name.replace(".", "-"),
+            sms_domain_id=config["sagemaker_studio_domain_id"],
+            startup_script=startup_script,
+        )
+        client.chat_postMessage(
+            channel=user_id, text=f"EC2 instance {instance} launched successfully."
+        )
+    except ec2_client.exceptions.ClientError as e:
+        client.chat_postMessage(
+            channel=user_id,
+            text=f"Error launching EC2 instance: {e.response['Error']['Message']}",
+        )
+
+
+def handle_instance_termination(user_id, instance_ids):
+    try:
+        ec2_client.terminate_instances(InstanceIds=instance_ids)
+        terminated_instances = ", ".join(instance_ids)
+        client.chat_postMessage(
+            channel=user_id, text=f"Terminated instances: {terminated_instances}"
+        )
+    except ec2_client.exceptions.ClientError as e:
+        client.chat_postMessage(
+            channel=user_id,
+            text=f"Error terminating instances: {e.response['Error']['Message']}",
+        )
+
+
+if __name__ == "__main__":
+    app.run(port=3000)
---
+++ ec2.py
@@ -0,0 +1,90 @@
+from textwrap import dedent
+from typing import List, Optional
+
+import boto3
+
+
+def launch_ec2_instance(
+    ec2_client: boto3.client,
+    ami_id: str,
+    iam_instance_profile: str,
+    instance_type: str,
+    key_name: str,
+    security_group_ids: List[str],
+    subnet_id: str,
+    efs_ip: Optional[str] = None,
+    sms_user_name: Optional[str] = None,
+    sms_domain_id: Optional[str] = None,
+    startup_script: str = None,
+) -> str:
+    user_data_script = dedent(
+        """\
+        #!/bin/bash
+    """
+    )
+
+    if efs_ip is not None and sms_user_name is not None and sms_domain_id is not None:
+        user_id = boto3.client("sagemaker").describe_user_profile(
+            DomainId=sms_domain_id, UserProfileName=sms_user_name
+        )["HomeEfsFileSystemUid"]
+
+        user_data_script += dedent(
+            f"""\
+            USER=ubuntu
+            HOME=/home/$USER
+
+            # Install NFS client
+            sudo apt-get update
+            sudo DEBIAN_FRONTEND=noninteractive apt-get install nfs-common -y
+
+            # Move the authorized_keys file out of the way
+            sudo mv $HOME/.ssh/authorized_keys /tmp/authorized_keys
+
+            # Mount the EFS file system
+            sudo groupmod -g 1001 users
+            sudo usermod -u {user_id} -g users $USER
+            echo "{efs_ip}:/{user_id} $HOME nfs nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" | sudo tee -a /etc/fstab
+            sudo mount $HOME
+
+            # Merge the authorized_keys files
+            sudo touch $HOME/.ssh/authorized_keys
+            sudo chmod 600 $HOME/.ssh/authorized_keys
+            sudo cat /tmp/authorized_keys $HOME/.ssh/authorized_keys | sort | uniq | sudo tee $HOME/.ssh/authorized_keys
+            sudo chown ubuntu:users $HOME/.ssh/authorized_keys
+
+            # Install Docker engine
+            sudo mkdir -p /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+            sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+            sudo usermod -aG docker $USER
+            sudo systemctl enable docker
+            """
+        )
+
+    if startup_script is not None:
+        user_data_script += startup_script
+
+    response = ec2_client.run_instances(
+        IamInstanceProfile={"Name": iam_instance_profile},
+        ImageId=ami_id,
+        InstanceType=instance_type,
+        KeyName=key_name,
+        MinCount=1,
+        MaxCount=1,
+        SecurityGroupIds=security_group_ids,
+        SubnetId=subnet_id,
+        TagSpecifications=[
+            {
+                "ResourceType": "instance",
+                "Tags": [
+                    {"Key": "User", "Value": key_name},
+                ],
+            }
+        ],
+        UserData=user_data_script,
+    )
+
+    instance_id = response["Instances"][0]["InstanceId"]
+    return instance_id
