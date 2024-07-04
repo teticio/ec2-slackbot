99f0b649a2b4fbb6a6d07653da88a19e82ae9c52
Robert Dargavel Smith <teticio@gmail.com>
2024-07-04 20:32:09 +0100

    pass in call_count

--- tests/test.py
+++ tests/test.py
@@ -214,28 +214,28 @@ class TestSlackHandler(unittest.TestCase):
         """
         Test operations on instances and volumes in order.
         """
-        self._test_ec2_key(mock_views_open)
+        self._test_ec2_key(mock_views_open, call_count=1)
         self._test_create_public_key(mock_chat_post_message)
-        self._test_open_instance_launch_modal(mock_views_open)
+        self._test_open_instance_launch_modal(mock_views_open, call_count=2)
         self._test_launch_instance(mock_chat_post_message)
-        self._test_ec2_down(mock_views_open)
-        self._test_ec2_stop(mock_views_open)
+        self._test_ec2_down(mock_views_open, call_count=3)
+        self._test_ec2_stop(mock_views_open, call_count=4)
         self._test_stop_instance(mock_chat_post_message)
-        self._test_ec2_start(mock_views_open)
+        self._test_ec2_start(mock_views_open, call_count=5)
         self._test_start_instance(mock_chat_post_message)
-        self._test_ec2_change(mock_views_open)
+        self._test_ec2_change(mock_views_open, call_count=6)
         self._test_change_instance_type(mock_chat_post_message)
-        self._test_ebs_create(mock_views_open)
+        self._test_ebs_create(mock_views_open, call_count=7)
         self._test_create_volume(mock_chat_post_message)
-        self._test_ebs_resize(mock_views_open)
+        self._test_ebs_resize(mock_views_open, call_count=8)
         self._test_resize_volume(mock_chat_post_message)
-        self._test_ebs_attach(mock_views_open)
+        self._test_ebs_attach(mock_views_open, call_count=9)
         self._test_attach_volume(mock_chat_post_message)
         self._test_detach_volume(mock_chat_post_message)
         self._test_destroy_volume(mock_chat_post_message)
         self._test_terminate_instance(mock_chat_post_message)
 
-    def _test_ec2_key(self, mock_views_open: Mock) -> None:
+    def _test_ec2_key(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ec2 key command.
         """
@@ -244,7 +244,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "key"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        mock_views_open.assert_called_once()
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_create_public_key(self, mock_chat_post_message: Mock) -> None:
         """
@@ -273,7 +273,9 @@ class TestSlackHandler(unittest.TestCase):
             channel=self.user_id, text="Public key updated successfully."
         )
 
-    def _test_open_instance_launch_modal(self, mock_views_open: Mock) -> None:
+    def _test_open_instance_launch_modal(
+        self, mock_views_open: Mock, call_count: int
+    ) -> None:
         """
         Test opening the instance launch modal.
         """
@@ -281,7 +283,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "up"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 2)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_launch_instance(self, mock_chat_post_message: Mock) -> None:
         """
@@ -315,7 +317,7 @@ class TestSlackHandler(unittest.TestCase):
         match = re.search(r"i-[0-9a-fA-F]{17}", self.text)
         self.instance_id = match.group(0) if match else ""
 
-    def _test_ec2_down(self, mock_views_open: Mock) -> None:
+    def _test_ec2_down(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ec2 down command.
         """
@@ -323,9 +325,9 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "down"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 3)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
-    def _test_ec2_stop(self, mock_views_open: Mock) -> None:
+    def _test_ec2_stop(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ec2 stop command.
         """
@@ -333,7 +335,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "stop"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 4)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_stop_instance(self, mock_chat_post_message: Mock) -> None:
         """
@@ -361,7 +363,7 @@ class TestSlackHandler(unittest.TestCase):
             channel=self.user_id, text=f"Stopped instances: {self.instance_id}"
         )
 
-    def _test_ec2_start(self, mock_views_open: Mock) -> None:
+    def _test_ec2_start(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ec2 start command.
         """
@@ -369,7 +371,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "start"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 5)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_start_instance(self, mock_chat_post_message: Mock) -> None:
         """
@@ -397,7 +399,7 @@ class TestSlackHandler(unittest.TestCase):
             channel=self.user_id, text=f"Started instances: {self.instance_id}"
         )
 
-    def _test_ec2_change(self, mock_views_open: Mock) -> None:
+    def _test_ec2_change(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ec2 change command.
         """
@@ -405,7 +407,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "change"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 6)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_change_instance_type(self, mock_chat_post_message: Mock) -> None:
         """
@@ -437,7 +439,7 @@ class TestSlackHandler(unittest.TestCase):
             text=f"Changed instance {self.instance_id} to type t3.medium successfully.",
         )
 
-    def _test_ebs_create(self, mock_views_open: Mock) -> None:
+    def _test_ebs_create(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ebs create command.
         """
@@ -446,7 +448,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "create"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 7)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_create_volume(self, mock_chat_post_message: Mock) -> None:
         """
@@ -468,7 +470,7 @@ class TestSlackHandler(unittest.TestCase):
             channel=self.user_id, text="EBS volume of 1 GiB created successfully."
         )
 
-    def _test_ebs_resize(self, mock_views_open: Mock) -> None:
+    def _test_ebs_resize(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ebs resize command.
         """
@@ -476,7 +478,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "resize"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 8)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_resize_volume(self, mock_chat_post_message: Mock) -> None:
         """
@@ -502,7 +504,7 @@ class TestSlackHandler(unittest.TestCase):
             ),
         )
 
-    def _test_ebs_attach(self, mock_views_open: Mock) -> None:
+    def _test_ebs_attach(self, mock_views_open: Mock, call_count: int) -> None:
         """
         Test the /ebs attach command.
         """
@@ -510,7 +512,7 @@ class TestSlackHandler(unittest.TestCase):
         self.command_payload["text"] = "attach"
         response = self.post_command(self.command_payload, timeout=0)
         self.assertEqual(response.text, "")
-        self.assertEqual(mock_views_open.call_count, 9)
+        self.assertEqual(mock_views_open.call_count, call_count)
 
     def _test_attach_volume(self, mock_chat_post_message: Mock) -> None:
         """
